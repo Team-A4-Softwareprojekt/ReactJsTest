183321b4eec101555c53037804b6609f31f8a9d3
oliver15022003 <oliver.mann007@gmail.com>
2024-04-25 15:22:42 +0200

    added a Card Component with our Logo

---
+++ react-app-test/src/Card.jsx
@@ -0,0 +1,13 @@
+import logoImg from './assets/logo.png'
+
+function Card(){
+    return(
+        <div className = "card">
+            <img src= {logoImg} alt="logo" className='card-logo'></img>
+            <h2 className='card-title'>Senpai</h2>
+            <p className='card-text'>Super Coole und Inovative Lernplattform</p>
+        </div>
+    );
+}
+
+export default Card
